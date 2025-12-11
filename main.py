40fccdcd100ef7bdc40b531cf5ebc451e037d22a
Simon <simon.gonzalez@edu.ece.fr>
2025-12-11 13:15:17 +0900

    je teste là

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
 # main.py
-from PySide6.QtWidgets import QApplication
-from ui_main import MainWindow
-from db import init_db
-import sys
-
-if __name__ == "__main__":
-    init_db()
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+print("hello world")
